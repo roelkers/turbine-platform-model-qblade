0228a1c2f66eb22a4de9259618a9eadeb56fdbb6
Rufus Oelkers <oelkers@lorm.hfi.lan>
2018-04-18 11:05:05 +0200

    change moooringline.h to not use namespaces

--- MooringLine.h
+++ MooringLine.h
@@ -1,20 +1,18 @@
 #ifndef MOORINGLINE_H
 #define MOORINGLINE_H
 
+#include "chrono_fea/ChBuilderBeam.h"
 #include "chrono/physics/ChSystem.h"
 #include "chrono/physics/ChBodyEasy.h"
 #include "chrono_fea/ChMesh.h"
 
 #include "PlatformParams.h"
 
-using namespace chrono;
-using namespace chrono::fea;
-
 class MooringLine{
 private:
-  ChBuilderBeamANCF builder;
+  chrono::fea::ChBuilderBeamANCF builder;
 public:
-  MooringLine(ChSystem& system, std::shared_ptr<ChMesh> mesh, PlatformParams p, double theta, std::shared_ptr<ChBodyEasyCylinder> monopile);
+  MooringLine(chrono::ChSystem& system, std::shared_ptr<chrono::fea::ChMesh> mesh, PlatformParams p, double theta, std::shared_ptr<chrono::ChBodyEasyCylinder> monopile);
   void render();
 };
 
